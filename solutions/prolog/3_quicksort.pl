3f32f87240df75b0d89cca15e06fb71b410b4484
Vadim Sychev <vksychev@yandex.ru>
2019-12-02 18:43:17 +0300

    quicksort

--- solutions/prolog/3_quicksort.pl
+++ solutions/prolog/3_quicksort.pl
@@ -1,9 +1,13 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
 
-qsort(L,K):-i_sort(L,[],K).
-i_sort([],Ac,Ac).
-i_sort([H|T],Ac,R):-insert(H,Ac,Ac1),i_sort(T,Ac1,R).
+apnd( [], X, X).                                   
+apnd( [X | Y], Z, [X | W]) :- apnd( Y, Z, W).
+
+qsort([],[]).
+qsort([H|T],R):-
+	pivoting(H,T,L1,L2),qsort(L1,R1),qsort(L2,R2),
+	apnd(R1,[H|R2], R).
    
-insert(X,[Y|T],[Y|T1]):-X>Y,insert(X,T,T1).
-insert(X,[Y|T],[X,Y|T]):-X=<Y.
-insert(X,[],[X]).
+pivoting(H,[],[],[]).
+pivoting(H,[X|T],[X|L],G):-X=<H,pivoting(H,T,L,G).
+pivoting(H,[X|T],L,[X|G]):-X>H,pivoting(H,T,L,G).
